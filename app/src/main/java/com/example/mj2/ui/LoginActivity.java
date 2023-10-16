95abe7af00274c7f9a26509271ec018efbe12912
Abdullah Amin <bido6573@gmail.com>
2023-10-16 22:12:33 +0300

    finish login + register

--- app/src/main/java/com/example/mj2/ui/LoginActivity.java
+++ app/src/main/java/com/example/mj2/ui/LoginActivity.java
@@ -2,15 +2,69 @@ package com.example.mj2.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mj2.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private EditText emailET, passwordET;
+
+    private int counter = 0;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+    }
+
+    public void login(View view) {
+        counter++;
+        if (counter > 2) {
+            view.setEnabled(false);
+            MaterialButton btn = findViewById(R.id.loginBtn);
+            btn.setTextColor(Color.GRAY);
+            Toast.makeText(this, "Login os disabled", Toast.LENGTH_SHORT).show();
+        }
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        //password = Asdfghjkl;'1_
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Fields can not be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Log.i(TAG, "abdo: success");
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    }
+                });
+    }
+
+    public void goToRegisterPage(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/mj2/ui/RegisterActivity.java
@@ -0,0 +1,88 @@
+package com.example.mj2.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mj2.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText nameET, phoneET, emailET, passwordET;
+
+    private static final String TAG = "RegisterActivity";
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        auth = FirebaseAuth.getInstance();
+
+        nameET = findViewById(R.id.nameET);
+        phoneET = findViewById(R.id.phoneET);
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+    }
+
+    public void register(View view) {
+        String name = nameET.getText().toString().trim();
+        String phone = phoneET.getText().toString().trim();
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        if (!isValidPassword(password)) {
+            Toast.makeText(this, "Invalid password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (name.isEmpty() || phone.isEmpty()
+                || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Fields can not be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> {
+                    Log.i(TAG, "abdo: success");
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    finish();
+                });
+    }
+
+    public void backToLoginPage(View view) {
+    }
+
+    public boolean isValidPassword(final String password) {
+
+        Pattern pattern;
+        Matcher matcher;
+
+        final String PASSWORD_PATTERN = "^" +
+                "(?=.*[0-9])" +
+                "(?=.*[a-z])" +
+                "(?=.*[A-Z])" +
+                "(?=.*[@#$_%^&+=-])" +
+                "(?=\\S+$).{8,}$";
+
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
+    }
+}
